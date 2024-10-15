5eaa00a498d013578ffd54656b322c2e121dd709
baka <c.durai.delhi@gmail.com>
2024-10-15 22:36:36 +0530

    added two print line statements

--- TestOne.java
+++ TestOne.java
@@ -2,6 +2,8 @@ public class TestOne
 {
 	public static void main(String[] args)
 	{
+		System.out.println("Start of main thread");
 		System.out.println("Testing!!");
+		System.out.println("End of main thread");
 	}
 }

3b74aac85d0ef6d35c1c9c25f3ea8964327553d3
D.V.BALA  SUBRAMANIAM <52366752+BalaS13@users.noreply.github.com>
2024-11-01 20:30:35 +0530

    Update index.js second time

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ console.log("Start of Anonymous Function");
 console.log("I have created this index.js file directly on the Github");
 const author = "Linus Torvald";
 console.log(` Creator of Git : ${author} `);
+console.log("Trying to print the age of the author");
+let age = 60;
+console.log(` Age of the Author is : ${age} `);
 console.log("End of Anonymous Function");
 

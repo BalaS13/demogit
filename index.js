3d55a1d2f344e7ef32c08f92051e1261d6a36f9c
D.V.BALA  SUBRAMANIAM <52366752+BalaS13@users.noreply.github.com>
2024-11-01 20:21:04 +0530

    Update index.js file!

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+console.log("Start of Anonymous Function");
 console.log("I have created this index.js file directly on the Github");
+const author = "Linus Torvald";
+console.log(` Creator of Git : ${author} `);
+console.log("End of Anonymous Function");
+
